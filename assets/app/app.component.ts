5b42929a09c01ebf4ba223926f00dcadd57e69b1
Anderson Mejia <ander1298@gmail.com>
2017-12-14 16:23:00 -0500

    Added new components, added property binding and directive binding

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
+import { Message } from './messages/message.model';
 
 @Component({
     selector: 'my-app',
-    templateUrl: './app.component.html'
+    templateUrl: './app.component.html'    
 })
 export class AppComponent {
-    message = {
-        content : 'A message',
-        author : 'Ander'
-    }
+    message : Message = new Message('Hi everybody', 'Anderson Mejia');
 }
