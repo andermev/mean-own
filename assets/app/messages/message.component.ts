f36af9677861430ea00b685ed0f6753e24e057de
Anderson Mejia <ander1298@gmail.com>
2017-12-14 19:34:50 -0500

    Added delete function

--- assets/app/messages/message.component.ts
+++ assets/app/messages/message.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, Output, EventEmitter } from "@angular/core";
 import { Message } from "./message.model";
+import { MessageService } from "./message.service";
 
 @Component({
     selector: 'app-message',
@@ -26,7 +27,15 @@ export class MessageComponent{
     //attribute binding html tag
     //color = 'red';
 
+    constructor(private messageService : MessageService){
+
+    };
+
     onEdit(){
         this.editClicked.emit('A new event');
+    }
+
+    onDelete(){
+        this.messageService.deleteMessage(this.message);
     }
 }
